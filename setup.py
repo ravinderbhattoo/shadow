0c9be90a648cfe6450413209687e19b4aff022e6
Ravinder Bhattoo <ravinderbhattoo@gmail.com>
2021-05-30 04:21:20 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='shadow',
+   version='0.0.0',
+   description='Custom plots using matplotlib.',
+   author='Ravinder Bhattoo',
+   author_email='',
+   packages=['shadow'],
+   install_requires=['matplotlib', 'scipy', 'numpy']
+)
